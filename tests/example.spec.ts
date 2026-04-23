2aea72b904855fe5bd1567b27cbc3c23df753352
WoftGang011 <thaiphong011199@gmail.com>
2026-04-23 10:46:28 +0700

    Cập nhật các file test cho Playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
 
+
 test('Verify Logo and Title', async ({ page }) => {
   await page.goto('https://qa.b3.works/');
 
-  await page.pause();
+  //await page.pause();
   // Expect a title "to contain" a substring.
   //await expect(page).toHaveTitle(/Playwright/);
 
+  // Check logo + title + login
   const logo = page.getByAltText('logo');
 
   await expect(logo).toBeVisible({ timeout: 15000 });
@@ -51,12 +53,141 @@ test('Verify Logo and Title', async ({ page }) => {
 
   await signInBtn.click();
 
-  const listOrg = page.locator('.org-item');
-
-
+  // Select org
   const specificOrg = page.locator('.org-item').filter({ hasText: 'CPaaS QA Account' });
 
   await specificOrg.click();
+
+  // Click avatar menu
+  const avatarBtn = page.locator('[mattooltip = "Profile"]');
+
+  await avatarBtn.hover();
+
+  const tooltip = page.locator('.cdk-overlay-container');
+
+  await expect(tooltip.getByText('Profile')).toBeTruthy();
+
+  const userProfile = page.locator('.user-profile');
+
+  await expect(userProfile).toBeVisible({ timeout: 15000 });
+
+  await userProfile.click();
+
+
+  // const statusMenuTrigger = page.getByRole('menuitem', { name: 'Available' }).first();
+
+  // await statusMenuTrigger.hover();
+
+  // const subMenuContainer = page.locator('.mat-mdc-menu-pannel');
+  // // await expect(subMenuContainer).toBeVisible({ timeout: 5000 });
+
+  // const statuses = ['Available', 'Busy', 'Away', 'Offline'];
+
+  // for (const statusName of statuses) {
+
+  //   const statusItem = subMenuContainer.getByRole('menuitem', { name: new RegExp(statusName, 'i') });
+
+  //   await expect(statusItem).toBeVisible({ timeout: 5000 });
+
+  //   console.log('đã kiểm tra: ${statusName} - hiển thị OK');
+  // }
+
+  // Check manage org redirect & click system rules 
+  const manageOrg = page.getByRole('menuitem', { name: "Manage organization" });
+
+  //const manageOrg = page.locator('.mat-mdc-menu-item').filter({ hasText: 'Manage organization' });
+
+  await manageOrg.click({ timeout: 15000 });
+
+  const manageOrgIframe = page.frameLocator('#db6d1b97-fed8-4fb4-b4c5-b333d703f579_manage-organization');
+
+  //await expect(page.getByRole('button', { name: 'System rules' })).toBeVisible({ timeout: 15000 });
+
+
+  const systemRulesBtn = manageOrgIframe.getByRole('button', { name: 'System rules' });
+
+  await expect(systemRulesBtn).toBeVisible({ timeout: 15000 });
+
+  await systemRulesBtn.click();
+
+
+  //const outboundCallRules = manageOrgIframe.locator('div.link').filter({ hasText: 'Outbound call rules' });
+  const outboundCallRules = manageOrgIframe.getByText('Outbound call rules');
+  await expect(outboundCallRules).toBeVisible({ timeout: 15000 });
+
+  await outboundCallRules.click();
+
+
+  const customRuleBtn = manageOrgIframe.getByText('Custom rule');
+
+  await expect(customRuleBtn).toBeVisible({ timeout: 15000 });
+
+  await customRuleBtn.click({ timeout: 15000 });
+
+
+  const createBtn = manageOrgIframe.locator('.mdc-button');
+
+  await expect(createBtn).toBeVisible({ timeout: 15000 });
+
+  await expect(createBtn).toBeEnabled();
+
+  await createBtn.click({ timeout: 15000 });
+
+
+
+
+
+  // const popupOutboundCallRule = manageOrgIframe.locator('.mat-mdc-dialog-container');
+
+  // await expect(popupOutboundCallRule).toBeVisible({ timeout: 15000 });
+
+  // Create new rule
+  const createOutboundCallRule = manageOrgIframe.locator('input[formcontrolname="name"]');
+
+  await expect(createOutboundCallRule).toBeVisible({ timeout: 15000 });
+
+  await createOutboundCallRule.fill('Test rule 01');
+
+  const createBtn2 = manageOrgIframe.getByRole('button', { name: "Create" });
+
+  await expect(createBtn2).toBeVisible({ timeout: 15000 });
+
+  await expect(createBtn2).toBeEnabled();
+
+  await createBtn2.click({ timeout: 15000 });
+
+
+  // Close popup
+  const closeBtn = manageOrgIframe.getByRole('button', { name: 'Close' });
+
+  await expect(closeBtn).toBeVisible({ timeout: 15000 });
+
+  await expect(closeBtn).toBeEnabled();
+
+  await closeBtn.click({ timeout: 15000 });
+
+
+  // search field 
+
+  const searchField = manageOrgIframe.getByPlaceholder('Rule name');
+
+  await expect(searchField).toBeVisible({ timeout: 15000 });
+
+  await searchField.fill('Test rule 01');
+
+  // await createBtn2.click({ timeout: 15000 });
+
+  // edit rule
+
+  await page.pause();
+
+  const hoverRow = manageOrgIframe.getByRole('row', { name: 'Test rule 01' });
+
+  await hoverRow.hover({ timeout: 15000 });
+
+  const editBtn = hoverRow.locator('button[mattooltip="Update"]');
+  await expect(editBtn).toBeVisible({ timeout: 15000 });
+  await editBtn.click({ timeout: 15000 });
 
 });
 
---
+++ tests/mytest.ts
@@ -0,0 +1,32 @@
+/*
+
+
+
+page.getByRole() to locate by explicit and implicit accessibility attributes.
+page.getByText() to locate by text content.
+page.getByLabel() to locate a form control by associated label's text.
+page.getByPlaceholder() to locate an input by placeholder.
+page.getByAltText() to locate an element, usually image, by its text alternative.
+page.getByTitle() to locate an element by its title attribute.
+page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
+
+
+*/
+
+
+
+import { test, expect } from "@playwright/test";
+
+test("Verify Playwright Locator", async ({ page }) => {
+
+    await page.goto("https://demo.nopcommerce.com/");
+
+    await page.pause();
+
+    const logo = page.getByAltText("nopCommerce demo store");
+
+    await expect(logo).toBeVisible();
+
+
+
+})
